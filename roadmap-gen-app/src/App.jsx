fa245318c7ff40ca78f2147cefa92e7b275f592f
sangarananthan <sangarsmart16@gmail.com>
2024-11-24 20:42:42 +0530

    Analytics added

--- roadmap-gen-app/src/App.jsx
+++ roadmap-gen-app/src/App.jsx
@@ -1,9 +1,10 @@
 import Home from "./pages/Home";
-
+import { Analytics } from "@vercel/analytics/react";
 const App = () => {
   return (
     <>
       <Home />
+      <Analytics />
     </>
   );
 };
